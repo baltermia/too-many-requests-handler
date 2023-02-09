bde227031648067a94db14dfe4985a10bde03a37
baltermia <54800864+baltermia@users.noreply.github.com>
2023-02-09 11:42:38 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# too-many-requests-handler
-A simple HttpClientHandler which handles HTTP 429 (TooManyRequest) and also allows parallel pausing.
+# TooManyRequestsHandler
+> A simple HttpClientHandler which handles HTTP 429 (TooManyRequest) and also allows parallel pausing.
+
+## How to Use
+
+### Using `IHttpClientFactory`
+
+```csharp
+IHost host = Host.CreateDefaultBuilder()
+
+                .ConfigureAppConfiguration( /* Configure your app-config */)
+
+                .ConfigureLogging(/* Configure your logging */)
+
+                .ConfigureServices(services =>
+                    services
+                        .AddHttpClient()
+
+                        // Here is where you add the handler
+                        .ConfigurePrimaryHttpMessageHandler(_ => new TooManyRequestsHandler
+                        {
+                            // Any settings you want (since HttpClientHandler is derived from)
+                        })
+                        
+                        .ConfigureHttpClient((_, client) => 
+                        {
+                            client.DefaultRequestHeaders.Accept.Add(new("application/json"));
+                            // might also add other settings
+                        }))
+                .Services
+                .Build();
+```
+
+
+### In `HttpClient`-Constructor
+
+Simply provide the `TooManyRequestsHandler` when constructing a new `HttpClient`:
+```csharp
+TooManyRequestsHandler handler = new 
+{
+    // Any settings you want (since HttpClientHandler is derived from)
+};
+
+HttpClient client = new(handler);
+```
+
+## Compatibility
+
+The library supports the following .NET versions:
+- .NET 5+
+- .NET Standard 2.1+
+
+All other versions `StatusCode.TooManyRequests` is not avaliable, therefore the library will not be supported for those versions.
+
+## Inconveniences
+
+> This explains why you might get "The request was canceled due to the configured HttpClient.Timeout of 100 seconds elapsing." exception messages.
+
+### Fix
+
+To fix the problem, simply set the `HttpClient.Timeout` property to a greated value or infinite ([`TimeSpan.InfiniteTimeSpan`](https://learn.microsoft.com/en-us/dotnet/api/system.threading.timeout.infinitetimespan), or for < .NET 7 `TimeSpan.FromMilliseconds(-1)`;
+
+
+```csharp
+// For >= .NET 7
+TimeSpan timeout = TimeSpan.InfiniteTimeSpan;
+
+// For < .NET 7
+TimeSpan timeout = TimeSpan.FromMilliseconds(-1);
+
+// Using IHttpClientFactory
+.ConfigureHttpClient((_, client) => 
+{
+    client.Timeout = timeout;
+})
+
+// Or using constructor
+HttpClient client = new(handler)
+{
+    Timeout = timeout
+};
+```
+
+### Explanation
+
+The `HttpClient` class holds a `Timeout` property, which by default is set to 100 seconds. (More about it [here](https://learn.microsoft.com/en-us/dotnet/api/system.net.http.httpclient.timeout#system-net-http-httpclient-timeout))
+
+That Timeout has nothing to do with requests directly, but rather with how long the `HttpClient`s underlying `HttpMessageHandler` takes. And since that handler in our case asynchronously waits until the time in the `HttpResponseMessage.Headers.RetryAfter.Date` is reached, the time for a request will rise with each retry.
+
+It's a very unfortunate problem and I haven't found a good fix for it yet (not setting the Timeout to infinite), but I would be glad if someone could help me fix it!
---
+++ TooManyRequestsHandler/TooManyRequestsHandler.cs
@@ -0,0 +1,109 @@
+﻿using System.Net;
+using Nito.AsyncEx;
+
+namespace TooManyRequestsHandler;
+
+/// <summary>
+/// Handles HTTP 429 Requests and automatically retries after the returned time. Will also handle parallel calls.
+/// Note that the Time per each request gets very high. Consider setting <see cref="HttpClient.Timeout" /> to a <see cref="TimeSpan"/> of -1ms (infinite) 
+/// </summary>
+public class TooManyRequestsHandler : HttpClientHandler
+{
+    private readonly PauseTokenSource _pauseSource = new();
+
+    public PauseToken PauseToken { get; }
+
+    private readonly AsyncLock _asyncLock = new();
+
+#if NET5_0_OR_GREATER
+    private readonly object _syncLock = new();
+#endif
+
+    public TooManyRequestsHandler()
+    {
+        PauseToken = _pauseSource.Token;
+    }
+
+#if NET5_0_OR_GREATER
+    protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        PauseToken.WaitWhilePaused(cancellationToken);
+
+        HttpResponseMessage result = base.Send(request, cancellationToken);
+
+        if (result.StatusCode != HttpStatusCode.TooManyRequests)
+            return result;
+
+        lock (_syncLock)
+        {
+            DateTimeOffset? time = result.Headers.RetryAfter?.Date;
+
+            TimeSpan delay = time - DateTimeOffset.UtcNow ?? TimeSpan.Zero;
+
+            if (delay > TimeSpan.Zero)
+            {
+                try
+                {
+                    _pauseSource.IsPaused = true;
+
+                    Thread.Sleep(delay);
+                }
+                finally
+                {
+                    _pauseSource.IsPaused = false;
+                }
+            }
+        }
+
+        return Send(request, cancellationToken);
+    }
+#endif
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        // check if requests are paused and wait
+        await PauseToken.WaitWhilePausedAsync(cancellationToken).ConfigureAwait(false);
+
+        HttpResponseMessage result = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+        // if result is anything but 429, return (even if it may is an error)
+        if (result.StatusCode != HttpStatusCode.TooManyRequests)
+            return result;
+
+        // create a locker which will unlock at the end of the stack
+        using IDisposable locker = await _asyncLock.LockAsync(cancellationToken).ConfigureAwait(false);
+
+        // calculate delay
+        DateTimeOffset? time = result.Headers.RetryAfter?.Date;
+        TimeSpan delay = time - DateTimeOffset.UtcNow ?? TimeSpan.Zero;
+
+        // if delay is 0 or below, return new requests
+        if (delay <= TimeSpan.Zero)
+        {
+            // very important to unlock
+            locker.Dispose();
+
+            // recursively recall itself
+            return await SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+
+        try
+        {
+            // otherwise pause requests
+            _pauseSource.IsPaused = true;
+
+            // then wait the calculated delay
+            await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
+        }
+        finally
+        {
+            _pauseSource.IsPaused = false;
+        }
+
+        // make sure to unlock again (otherwise the method would lock itself because of recurse)
+        locker.Dispose();
+
+        // recursively recall itself
+        return await SendAsync(request, cancellationToken).ConfigureAwait(false);
+    }
+}
